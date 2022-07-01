9aca8cf9f64d2906efdf9f460f9ea07856ab9fb9
sungchulyun <proattacker@naver.com>
2022-07-01 14:59:16 +0900

    nestjs 개요 및 디자인 패턴 실습 및 개발환경 세팅

---
+++ letsStart/dist/app.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var cats_route_1 = require("./cats/cats.route");
+var app = express();
+app.use(function (req, res, next) {
+    console.log(req.rawHeaders[1]);
+    console.log("this is logging middleware");
+    next();
+});
+app.use(express.json());
+app.use(cats_route_1.default);
+app.use(function (req, res, next) {
+    console.log("this is error middleware");
+    res.send({ error: "404 not find error" });
+});
+app.listen(8000, function () {
+    console.log("server is on...");
+});
+//# sourceMappingURL=app.js.map
