a73e770036f37427192dc0b0ea73f0744944d846
sungchulyun <proattacker@naver.com>
2022-07-13 09:09:10 +0900

    feat : 판매 글 게시 페이지 구현
    image 넣는거 해야됨

--- platform/src/boards/boards.controller.ts
+++ platform/src/boards/boards.controller.ts
@@ -4,25 +4,36 @@ import { Board } from './boards.entity';
 import { BoardsService } from './boards.service';
 import { Body, Controller, Get, Param, Post, Render, Req, Res, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { render } from 'nunjucks';
 
 @Controller('boards')
 export class BoardsController {
 
     constructor(private boardService: BoardsService){}
-
+                                                        //게시판 홈 페이지, 게시글 목록
     @Get('/')
     //@Render('boardhome.njk')
     async getAllBoard(@Res() res:Response){
         const boards = await this.boardService.getBoardAll();
-        console.log(boards);
         res.render('boardhome',  {boards:boards});
     }
+                                                            //게시판 검색
+                                                            /*
     @Get('/:id')
     getBoardById(@Param ('id') id:number): Promise<Board>{
         return this.boardService.getBoardById(id);
     }
-    @Post()
-    @Render('write.njk')
+                                                            */
+
+                                                            //게시판 글 작성 랜더링
+    @Get('/write')
+    //@Render('boardwrite.njk')
+    writeBoard(@Res() res:Response){
+        res.render('boardwrite');
+    }
+    
+                                                            //게시판 글 작성 POST
+    @Post('/writePro')
     @UsePipes(ValidationPipe)
     createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board>{
         return this.boardService.createBoard(createBoardDto);
--- platform/src/boards/dto/create-board.dto.ts
+++ platform/src/boards/dto/create-board.dto.ts
@@ -1,5 +1,10 @@
 /* eslint-disable prettier/prettier */
+import { IsNotEmpty } from "class-validator";
+
 export class CreateBoardDto{
+    @IsNotEmpty()
     title:string;
+
+    @IsNotEmpty()
     content:string;
 }
